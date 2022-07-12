1119ffaaaaff6a32453d54d4bacc4a578f441599
xavigu <xavigu91@gmail.com>
2022-07-12 17:17:46 +0200

    Install firebase and mui library, add Header component

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD8rAIsXLtE6yFrQrtGP0s35jqX5Pi2j8g",
+  authDomain: "clone-84b1f.firebaseapp.com",
+  projectId: "clone-84b1f",
+  storageBucket: "clone-84b1f.appspot.com",
+  messagingSenderId: "153996991411",
+  appId: "1:153996991411:web:7344b795a57a2d5c4e89c6"
+};
