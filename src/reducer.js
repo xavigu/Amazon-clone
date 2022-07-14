8dfea66ae78e63ce58d7e4eb0c429320b99afdab
xavigu <xavigu91@gmail.com>
2022-07-14 21:34:54 +0200

    Add checkoutProduct component, use in checkout component and add delete item functionality in the reducer

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,8 @@ export const initialState = {
   basket: []
 }
 
+export const getBasketTotal = (basket) => basket?.reduce((acc, current) => acc + current.price, 0);
+
 const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
@@ -9,6 +11,22 @@ const reducer = (state, action) => {
       return {
         ...state,
         basket: [...state.basket, action.item]
+      }
+    case 'REMOVE_FROM_BASKET':
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      // a copy of the basket to modifiy
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn('Cant remove product')
+      }
+      return {
+        ...state,
+        basket: newBasket
       }
     default:
       return state;
