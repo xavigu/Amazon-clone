f283b4196fc98f70d8d2eda00cb6a132151ea215
xavigu <xavigu91@gmail.com>
2022-07-14 17:51:01 +0200

    Use routes in project and add checkout page with two side components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Header />
         <Routes>
-          <Route path='/checkout' element = {[
-            <Header/>, 
-            <p>Select an invoice</p>
-          ]}/>
+          <Route path='/checkout' element = { <Checkout/> }/>
+          <Route path='/' element = { <Home/> } />
         </Routes>
-        <Route path='/' element = {[<Header/>, <Home/>]} />
       </div>
     </BrowserRouter>
   );
