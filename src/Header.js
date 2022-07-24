77740885e224d60e481494195b265a5e941abb5e
xavigu <xavigu91@gmail.com>
2022-07-24 20:16:55 +0200

    Add route orders in the header

--- src/Header.js
+++ src/Header.js
@@ -40,10 +40,12 @@ function Header() {
           </div>
         </Link>
 
-        <div className='header__option'>
-          <span className='header__optionLineOne'>Return</span>
-          <span className='header__optionLineTwo'>& Orders</span>
-        </div>
+        <Link to={user && '/orders'}>
+          <div className='header__option'>
+            <span className='header__optionLineOne'>Return</span>
+            <span className='header__optionLineTwo'>& Orders</span>
+          </div>
+        </Link>  
 
         <div className='header__option'>
           <span className='header__optionLineOne'>Your</span>
